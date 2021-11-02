49316f8ea230a0799e5297bb9b18097baa31624b
miguelkristian17 <Miguelkmartinez@gmail.com>
2021-11-02 13:32:11 -0700

    DB changes

--- config/db.js
+++ config/db.js
@@ -2,8 +2,10 @@ const mongoose = require("mongoose");
 const config = require("config");
 
 const connectDB = async () => {
+
+    
     try {
-        await mongoose.connect('mongodb://localhost:27017/local', {
+        await mongoose.connect(config.get("mongoURI"), {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
